ddc390463fe4c5f03827d2cfed24c9eb6bb2fe40
Edward Wang <edward.wang.95@gmail.com>
2016-03-04 01:16:11 -0500

    Working on Euler: Gozinta chains

---
+++ src/Euler/Euler.java
@@ -0,0 +1,10 @@
+package Euler;
+
+/**
+ * Created by edwardwang on 3/4/16.
+ */
+public class Euler {
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/TopCoder/TopCoder.java
@@ -0,0 +1,10 @@
+package TopCoder;
+
+/**
+ * Created by edwardwang on 3/4/16.
+ */
+public class TopCoder {
+    public static void main(String[] args){
+
+    }
+}
